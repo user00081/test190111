cbf00aea04751d449d5fc055ab6911d64f47e954
luca.palo@dos-group.com <luca.palo@dos-group.com>
2019-01-22 10:20:06 +0100

    modified params encoding

--- web/js/main.js
+++ web/js/main.js
@@ -1,17 +1,23 @@
 var Ajax = {
-    encodeJSONparams: function(arr) {
-        return JSON.stringify(arr);
+    encodePostParams: function(data) {
+        return typeof data == 'string' ? data : Object.keys(data).map(
+            function(k){ return encodeURIComponent(k) + '=' + encodeURIComponent(data[k]) }
+        ).join('&');
     },
     simpleSendRequest: function(params) {
         var xhttp =  window.XMLHttpRequest ? new XMLHttpRequest() : new ActiveXObject('Microsoft.XMLHTTP');
-        xhttp.open("POST", "http://heroku.devel", false);
+        xhttp.open("POST", "http://heroku.devel/endpoint.php", true);
+        xhttp.onreadystatechange = function() {
+            if (xhttp.readyState>3 && xhttp.status==200) { console.log(xhttp.responseText); }
+        };
+        xhttp.setRequestHeader('X-Requested-With', 'XMLHttpRequest');
         xhttp.setRequestHeader("Content-type", "application/x-www-form-urlencoded; charset=UTF-8");
         xhttp.send(params);
         console.log("From Ajax.simpleSendRequest(): "+params);
         console.log(xhttp);
     },
     performRequest: function(arr) {
-        var encodedParams = this.encodeJSONparams(arr);
+        var encodedParams = this.encodePostParams(arr);
         this.simpleSendRequest(encodedParams);
     }
 };
@@ -39,7 +45,9 @@ var CollectURLs = {
             var url2 = CollectURLs.getInputText("url2");
             if (CollectURLs.isURL(url1) && CollectURLs.isURL(url2)) {
                 e.preventDefault();
-                Ajax.performRequest("url1="+url1+"&url2="+url2);
+                //Ajax.performRequest("url1="+url1+"&url2="+url2);
+                paramObj = { url1: url1, url2: url2 }
+                Ajax.performRequest(paramObj);
                 console.log("From CollectURLS.attachEvent(): url1="+url1+"&url2="+url2);
             } else {
                 alert("Please check URL syntax before submitting compare request!");
