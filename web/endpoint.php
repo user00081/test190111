01fe66453fad8061ae79d7e9633c009981b24222
luca.palo@dos-group.com <luca.palo@dos-group.com>
2019-01-22 11:57:35 +0100

    added phpunit library

--- web/endpoint.php
+++ web/endpoint.php
@@ -1,2 +1,9 @@
 <?php
-echo "cucu";
+//echo "cucu";
+if (isset($_POST['url1']) )
+    echo "url1 direct from POST: ".$_POST['url1'];
+include_once 'includes/Listen.php';
+$listen = new Listen();
+$listen->savePostParam( 'url1' );
+
+echo "url1 from Listen class: ".$listen->getParamByProperty('url1');
---
+++ web/includes/Listen.php
@@ -0,0 +1,31 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: lucapalo
+ * Date: 22.01.19
+ * Time: 10:23
+ */
+
+class Listen
+{
+    protected $params;
+    public function __construct()
+    {
+        $this->params = new StdClass();
+    }
+    public function getParams() {
+        return $this->params;
+    }
+    public function getParamByProperty( $name ) {
+        return $this->params->{"$name"};
+    }
+    public function savePostParam( $name ) {
+        if ( isset( $_POST[$name]) )
+            $this->params->{ "$name" } = $_POST[$name];
+    }
+    public function saveAllPostVars() {
+        foreach ( $_POST as $var ) {
+            $this->savePostParam( $var );
+        }
+    }
+}
